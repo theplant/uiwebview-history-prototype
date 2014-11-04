427df25df2ea8a47d4f8c05443ccad1dd0507e2f
bodhi <bodhi@5263.org>
2014-11-04 23:54:19 +0900

    Add back button behaviour test

--- js-back-demo/ViewController.swift
+++ js-back-demo/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var backButton: UIButton!
+    @IBOutlet var webView: UIWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        loadContent(webView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func loadContent(webView : UIWebView) {
+        var url = NSBundle.mainBundle().URLForResource("index", withExtension:"html")
+        var request = NSURLRequest(URL: url!)
+
+        webView.loadRequest(request)
     }
 
+    @IBAction func goBack() {
+        if webView.canGoBack {
+            webView.goBack()
+        } else {
+            UIAlertView(title: "Close web view", message: "No more history for going back", delegate: nil, cancelButtonTitle: "OK").show()
+        }
+    }
 
 }
-
